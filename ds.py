18a4d75f0ac87cf1804744e94793c7dbeae0769d
ashoou9 <53280724+ashoou9@users.noreply.github.com>
2025-12-18 12:58:09 +0200

    Update ds.py

--- ds.py
+++ ds.py
@@ -2013,18 +2013,7 @@ else:
             
             st.markdown("####Commercial Excellence Team")
             st.markdown("""
-            • **Commercial Excellnece Director**
-                                                                                                 Dr- Khaled Gamal
-                                                   
-            • **Commercial Excellnece Manager 
-                        
-                                                Ms- Nahla Hassan**  
-            • **CNS** - Neuroscience Division  
-            • **GIT** - Gastroenterology  
-            • **Primary Care** - General Medicine  
-            • **CVM** - Cardiology Division  
-            • **Power Team** - Special Operations  
-            • **All Teams** - Comprehensive access
+            Striving for Excellence, Building the Future Together.   The Commercial Excellence Department, founded by Dr. Khaled Gamal in 2018, combines strategic process optimization with a culture of innovation. Alongside the steadfast support of Nahla Hassan, our team was energized in 2025 by the addition of iOS developer David, medical field expert Ali Mustafa, and computer science engineers Ahmed Attia and Asil. They have integrated with remarkable speed, imprinting the team with fresh creativity that heralds a highly promising 2026. Together, we work collaboratively to streamline operations, boost competitiveness, and secure market success.
             """)
             
             st.markdown("#### ✨ Features")
