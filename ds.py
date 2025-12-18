cac08d3744540614386960f2e7c240691940c668
ashoou9 <53280724+ashoou9@users.noreply.github.com>
2025-12-18 13:03:35 +0200

    Update ds.py

--- ds.py
+++ ds.py
@@ -2013,18 +2013,26 @@ else:
             
             st.markdown("Commercial Excellence Team")
             st.markdown("""
-            **Striving for Excellence,**
-                         **Building the Future Together.   The Commercial Excellence Department, founded by Dr. Khaled Gamal in 2018,**
-                         **combines strategic process optimization with a culture of innovation.**
-                         **Alongside the steadfast support of Nahla Hassan,**
-                         our team was energized in 2025 by the addition of iOS developer David,
-                         medical field expert Ali Mustafa,
-                         and computer science engineers Ahmed Attia and Asil.
-                         They have integrated with remarkable speed,
-                         imprinting the team with fresh creativity that heralds a highly promising 2026.
-                         Together, we work collaboratively to streamline operations, boost competitiveness,
-                         and secure market success.
+            **Striving for Excellence,**  
+
+            **Building the Future Together.**  
+
+            **The Commercial Excellence Department, founded by Dr. Khaled Gamal in 2018,**  
+            combines strategic process optimization with a culture of innovation.  
+
+            **Alongside the steadfast support of Nahla Hassan,**  
+            our team was energized in 2025 by the addition of iOS developer David,  
+            medical field expert Ali Mustafa,  
+            and computer science engineers Ahmed Attia and Asil.  
+
+            They have integrated with remarkable speed,  
+            imprinting the team with fresh creativity that heralds a highly promising 2026.  
+
+            Together, we work collaboratively to streamline operations,  
+            boost competitiveness,  
+            and secure market success.
             """)
+
             
             st.markdown("#### ✨ Features")
             st.markdown("""
